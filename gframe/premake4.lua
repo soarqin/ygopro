082b1a51d1eb5d001f92ad0cc78d3a3cd0449ec7
Damien Lawford <killerdamo@virginmedia.com>
2014-07-16 19:31:43 +0100

    irrKlang update

--- gframe/premake4.lua
+++ gframe/premake4.lua
@@ -5,7 +5,7 @@ project "ygopro"
 
     files { "**.cpp", "**.cc", "**.c", "**.h" }
     excludes "lzma/**"
-    includedirs { "../ocgcore", "../irrKlang-1.4.0/include" }
+    includedirs { "../ocgcore", "../irrKlang-1.5.0/include" }
     links { "ocgcore", "clzma", "Irrlicht", "freetype", "sqlite3", "lua", "event", "irrKlang" }
 
     configuration "windows"
@@ -14,7 +14,7 @@ project "ygopro"
         includedirs { "../irrlicht/include", "../freetype/include", "../event/include", "../sqlite3" }
         links { "opengl32", "ws2_32", "winmm", "gdi32", "kernel32", "user32", "imm32" }
     configuration {"windows", "vs*"}
-        libdirs { "../irrKlang-1.4.0/lib/Win32-visualStudio" }
+        libdirs { "../irrKlang-1.5.0/lib/Win32-visualStudio" }
     configuration {"windows", "not vs*"}
         includedirs { "/mingw/include/irrlicht", "/mingw/include/freetype2" }
     configuration "not vs*"
